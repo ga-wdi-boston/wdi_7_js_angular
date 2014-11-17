752bb1b99bb9ac750e8f861215617c5011edb2de
Dan Johnson <dan@balancerockmedia.com>
2014-11-17 08:20:13 -0500

    edits

--- demo6/js/app.js
+++ demo6/js/app.js
@@ -4,7 +4,7 @@ angular.module('Demo', []);
 // main controller
 angular.module('Demo').controller('MainCtrl', function($scope, $http) {
     'use strict';
-    
+
     $http.get('http://localhost:3000/users').success(function(response) {
         $scope.users = response;
     });
@@ -18,6 +18,19 @@ angular.module('Demo').controller('MainCtrl', function($scope, $http) {
             $scope.users.push(response);
 
             $scope.user = {};
+        });
+    };
+
+    $scope.deleteUser = function(user) {
+        $http.delete('http://localhost:3000/users/' + user.id).success(function(response) {
+            // remove from users array by id
+            for (var i = 0; i < $scope.users.length; i++){
+                if ($scope.users[i].id == user.id) {
+                    $scope.users.splice(i, 1);
+
+                    break;
+                }
+            }
         });
     };
 });
