65bb033a0472ddeb93fa92502d4e1f23ee27eda3
Dan Johnson <dan@balancerockmedia.com>
2014-11-20 12:28:40 -0500

    edits

--- demo13/js/app.js
+++ demo13/js/app.js
@@ -4,14 +4,19 @@ angular.module('Demo').controller('DemoCtrl', function($scope) {
     $scope.alertFeedback = function(feedback) {
         alert('Name: ' + feedback.name + '\nComments: ' + feedback.comments);
     };
+
+    $scope.foo = function(bar) {
+        alert(bar);
+    };
 });
 
 angular.module('Demo').directive('gaFeedback', function() {
     return {
         restrict: 'E',
         scope: {
-            placeholder: '@', // one way expression, outside in
-            submitAction: '&' // one way behavior, inside out, uses parent scope to execute
+            placeholder: '@', // one way property, outside in
+            submitAction: '&', // one way behavior, inside out, uses parent scope to execute
+            whatWhat: '&'
         },
         templateUrl: 'templates/feedback.html'
     };
