964614cdb3f4511cb340a9189dcf5814afbcd7a8
Dan Johnson <dan@balancerockmedia.com>
2014-11-20 11:22:50 -0500

    edits

--- demo11/js/app.js
+++ demo11/js/app.js
@@ -1,9 +1,22 @@
 angular.module('Demo', []);
 
 angular.module('Demo').directive('gaLorem', function() {
-    return {
+	return {
         restrict: 'EA',
 
-        template: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        templateUrl: 'templates/lorem.html',
+
+        scope: {
+            repetitions: '@' // one way, outside in
+        }
+    };
+});
+
+angular.module('Demo').filter('range', function() {
+	return function(param) {
+		if (param) {
+			// fancy way to fill an array with numbers 0..n
+			return Array.apply(null, {length: param}).map(Number.call, Number);
+        }
     };
 });
